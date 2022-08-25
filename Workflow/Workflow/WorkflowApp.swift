601bb34226f86c547730cbb10decf9f66469af1f
Daniel Stebbins <drs5972@psu.edu>
2022-08-24 20:20:40 -0400

    Changed app icons to all be Rick Astley.

---
+++ Workflow/Workflow/WorkflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkflowApp.swift
+//  Workflow
+//
+//  Created by Stebbins, Daniel Ross on 8/24/22.
+//
+
+import SwiftUI
+
+@main
+struct WorkflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
