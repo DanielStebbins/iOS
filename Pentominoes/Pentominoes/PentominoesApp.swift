d88084a70e78ad713d8695689133c7e9e5de5de6
Daniel Stebbins <drs5972@psu.edu>
2022-09-19 11:37:23 -0400

    Made the project and imported the provided assets and code.

---
+++ Pentominoes/Pentominoes/PentominoesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PentominoesApp.swift
+//  Pentominoes
+//
+//  Created by Stebbins, Daniel Ross on 9/19/22.
+//
+
+import SwiftUI
+
+@main
+struct PentominoesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ Pentominoes/Pentominoes/Views/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pentominoes
+//
+//  Created by Stebbins, Daniel Ross on 9/19/22.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
