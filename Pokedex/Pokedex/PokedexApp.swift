2a59549adba24b87b154dd58c5cc21bb42cdd11b
Daniel Stebbins <drs5972@psu.edu>
2022-10-24 13:10:13 -0400

    Model and ViewModel

--- Pokedex/Pokedex/PokedexApp.swift
+++ Pokedex/Pokedex/PokedexApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct PokedexApp: App {
+    @StateObject var manager = Manager()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(manager)
         }
     }
 }
---
+++ Pokedex/Pokedex/ViewModel/Manager.swift
@@ -0,0 +1,12 @@
+//
+//  Manager.swift
+//  Pokedex
+//
+//  Created by Stebbins, Daniel Ross on 10/24/22.
+//
+
+import Foundation
+
+class Manager: ObservableObject {
+    @Published var model = Model()
+}
