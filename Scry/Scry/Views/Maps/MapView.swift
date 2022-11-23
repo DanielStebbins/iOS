4256b2535c66a7a300ca71909634300affc4f999
Daniel Stebbins <drs5972@psu.edu>
2022-11-23 12:30:13 -0500

    Reorganizing Bubble related views and adding a MapView.

---
+++ Scry/Scry/Views/Maps/MapView.swift
@@ -0,0 +1,14 @@
+//
+//  MapView.swift
+//  Scry
+//
+//  Created by Thomas Stebbins on 11/23/22.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
