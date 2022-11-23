d6e0a0c5885f43de7f2e42e76e435689554748fc
Daniel Stebbins <drs5972@psu.edu>
2022-11-23 13:37:20 -0500

    Scrollable map image, currently with PDFKit, but subject to change to a lower-level zooming UIScrollView if PDFKit can't do everything I want it to in the future.

--- Scry/Scry/Views/Maps/MapView.swift
+++ Scry/Scry/Views/Maps/MapView.swift
@@ -5,10 +5,32 @@
 //  Created by Thomas Stebbins on 11/23/22.
 //
 
+// If the PDF view doesn't work for some reason in the future, try this UIKit solution:
+// https://stackoverflow.com/questions/58341820/isnt-there-an-easy-way-to-pinch-to-zoom-in-an-image-in-swiftui
+
 import SwiftUI
+import PDFKit
 
 struct MapView: View {
+    @ObservedObject var map: Map
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        PhotoDetailView(image: map.image != nil ? UIImage(data: map.image!)! : UIImage(imageLiteralResourceName: "square-grid"))
+    }
+}
+
+struct PhotoDetailView: UIViewRepresentable {
+    let image: UIImage
+
+    func makeUIView(context: Context) -> PDFView {
+        let view = PDFView()
+        view.document = PDFDocument()
+        guard let page = PDFPage(image: image) else { return view }
+        view.document?.insert(page, at: 0)
+        view.autoScales = true
+        return view
+    }
+
+    func updateUIView(_ uiView: PDFView, context: Context) {
+        // empty
     }
 }
